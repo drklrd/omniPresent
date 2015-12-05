1bc49b0fcddc988372853ed72ad2b650ecc7e164
saurav <saurav@zyoba.com>
2015-12-05 19:56:18 +0545

    adds test file

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/apis/common/signup',function(req,res,next){
+
+	res.json({
+		success : 1,
+		message : 'Successfully signed up !'
+	})
+
+});
+
+router.get('/test',function(req,res){
+
+		res.send('Test OK!')
+})
+
+
 module.exports = router;
