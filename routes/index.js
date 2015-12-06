fd3b2e69c70e274d0d0c5cd08556470965c8a6c7
saurav <saurav@zyoba.com>
2015-12-06 10:52:46 +0545

    another approach to DB connect

--- routes/index.js
+++ routes/index.js
@@ -13,45 +13,61 @@ router.post('/apis/common/signup', function(req, res, next) {
 
 	
 
-
 	var signUpUser = {
 		username : req.body.username,
 		password : req.body.password
-	}
+	};
 
-	pg.connect('postgres://jpktrpwpkpudcn:jhmwPUIJ2gDdyfP8hG8_Ejxg5N@ec2-107-21-219-109.compute-1.amazonaws.com:5432/d9j617e8naorrt', function(err, client, done) {
-		client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
-			done();
-			if (err) {
-				console.log(err)
-				return next(err);
-			} else {
+	var conString = 'postgres://jpktrpwpkpudcn:jhmwPUIJ2gDdyfP8hG8_Ejxg5N@ec2-107-21-219-109.compute-1.amazonaws.com:5432/d9j617e8naorrt';
 
-				var user = result;
-				if (result.length) {
-					res.json({
-						success: 0,
-						message: 'User Already exists ! Please login '
-					});
-				} else {
+	pg.connect(conString, function(err, client, done) {
+	  if(err) {
+	    console.error('error fetching client from pool', err);
+	    return  next(err);
+	  }
+	  client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
+	    done();
 
-					client.query("INSERT INTO users [username,password] VALUES('" + signUpUser.username + "','" + signUpUser.password + "')", function(err, result) {
-						done();
-						if (err) {
-							return next(err)
-						} else {
-							res.json({
-								success: 1,
-								message: 'Successfully signed up !'
-							});
-						}
-					});
-
-				}
-
-			}
-		});
+	    if(err) {
+	      return console.error('error running query', err);
+	    }
+	    console.log(result.rows[0].number);
+	  });
 	});
+
+	// pg.connect(, function(err, client, done) {
+	// 	client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
+	// 		done();
+	// 		if (err) {
+	// 			console.log(err)
+	// 			return next(err);
+	// 		} else {
+
+	// 			var user = result;
+	// 			if (result.length) {
+	// 				res.json({
+	// 					success: 0,
+	// 					message: 'User Already exists ! Please login '
+	// 				});
+	// 			} else {
+
+	// 				client.query("INSERT INTO users [username,password] VALUES('" + signUpUser.username + "','" + signUpUser.password + "')", function(err, result) {
+	// 					done();
+	// 					if (err) {
+	// 						return next(err)
+	// 					} else {
+	// 						res.json({
+	// 							success: 1,
+	// 							message: 'Successfully signed up !'
+	// 						});
+	// 					}
+	// 				});
+
+	// 			}
+
+	// 		}
+	// 	});
+	// });
 
 });
 
