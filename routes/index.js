bcc7eb23b5c0f051baee6932dc45cd2b0f456887
saurav <saurav@zyoba.com>
2015-12-06 22:09:38 +0545

    adds login api

--- routes/index.js
+++ routes/index.js
@@ -11,76 +11,109 @@ router.get('/', function(req, res, next) {
 
 router.post('/apis/common/signup', function(req, res, next) {
 
-	
-
 	var signUpUser = {
-		username : req.body.username,
-		password : req.body.password
+		username: req.body.username,
+		password: req.body.password
 	};
 
 	var conString = 'postgres://jpktrpwpkpudcn:jhmwPUIJ2gDdyfP8hG8_Ejxg5N@ec2-107-21-219-109.compute-1.amazonaws.com:5432/d9j617e8naorrt';
 
 	pg.connect(conString, function(err, client, done) {
-	  if(err) {
-	    console.error('error fetching client from pool', err);
-	    return  next(err);
-	  }
-	  client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
-	    done();
+		if (err) {
+			console.error('error fetching client from pool', err);
+			return next(err);
+		}
+		client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
+			done();
 
-	    if(err) {
-	      return console.error('error running query', err);
-	    }
-	    if(result.rows.length){
-	    	res.json({
-	    		success: 0,
-	    		message: 'User Already exists ! Please login '
-	    	});	
-	    }else{
+			if (err) {
+				return console.error('error running query', err);
+			}
+			if (result.rows.length) {
+				res.json({
+					success: 0,
+					message: 'User Already exists ! Please login '
+				});
+			} else {
 
-	    	client.query("INSERT INTO users (username,password) VALUES('" + signUpUser.username + "','" + signUpUser.password + "')", function(err, result) {
-	    		done();
-	    		if (err) {
-	    			return next(err)
-	    		} else {
-	    			res.json({
-	    				success: 1,
-	    				message: 'Successfully signed up !'
-	    			});
-	    		}
-	    	});
+				client.query("INSERT INTO users (username,password) VALUES('" + signUpUser.username + "','" + signUpUser.password + "')", function(err, result) {
+					done();
+					if (err) {
+						return next(err)
+					} else {
+						res.json({
+							success: 1,
+							message: 'Successfully signed up !'
+						});
+					}
+				});
 
-	    }
-	  });
+			}
+		});
 	});
 
-	// pg.connect(, function(err, client, done) {
-	// 	client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
-	// 		done();
-	// 		if (err) {
-	// 			console.log(err)
-	// 			return next(err);
-	// 		} else {
-
-	// 			var user = result;
-	// 			if (result.length) {
-					
-	// 			} else {
-
-					
-
-	// 			}
-
-	// 		}
-	// 	});
-	// });
 
 });
 
-router.get('/test', function(req, res) {
+router.post('/apis/common/signup', function(req, res, next) {
 
-	res.send('Test OK!')
-})
+
+
+	var user = {
+		username: req.body.username,
+		password: req.body.password
+	};
+
+	var conString = 'postgres://jpktrpwpkpudcn:jhmwPUIJ2gDdyfP8hG8_Ejxg5N@ec2-107-21-219-109.compute-1.amazonaws.com:5432/d9j617e8naorrt';
+
+	pg.connect(conString, function(err, client, done) {
+		if (err) {
+			console.error('error fetching client from pool', err);
+			return next(err);
+		}
+		client.query("SELECT * FROM users WHERE username='" + signUpUser.username + "'", function(err, result) {
+			done();
+
+			if (err) {
+				return next(err);
+			}
+			if (result.rows.length) {
+
+				var credential = result.rows[0].password;
+				if(credential ===  user.password) {
+
+					res.json({
+						success: 1,
+						message: 'Authorization Successful'
+					});
+
+				}else{
+
+					res.json({
+						success: 0,
+						message: ' Password doesnt match '
+					});
+
+				}
+
+
+				
+			} else {
+
+				res.json({
+					success: 0,
+					message: 'User Doesnt exists ! '
+				});
+
+				
+			}
+		});
+	});
+
+
+});
+
+
 
 
 module.exports = router;
